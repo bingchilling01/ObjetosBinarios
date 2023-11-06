eb72e00b0c541c72bccc06875ec2c19ff97a0425
Lin <126698830+bingchilling01@users.noreply.github.com>
2023-11-06 01:31:49 +0000

    ejercicio imperfecto

--- AccesoDirecto/src/accesodirecto/EjercicioAccDir.java
+++ AccesoDirecto/src/accesodirecto/EjercicioAccDir.java
@@ -1,5 +1,6 @@
 package accesodirecto;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -8,7 +9,7 @@ import utilidades.ES;
 public class EjercicioAccDir {
 
 	public static void main(String[] args) {
-		int contadorEstudiante = getNumeroElementos();
+
 		boolean salir = false;
 		int opcion;
 		while (!salir) {
@@ -21,145 +22,45 @@ public class EjercicioAccDir {
 			ES.msgln("6. Salir");
 
 			opcion = ES.leeEntero(1, 6);
-			
+
 			switch (opcion) {
 			case 1: {
-				contadorEstudiante++;
-				String nombreEstudiante = ES.leeCadena("Nombre del estudiante: ");
-				float nota = ES.leeFloat("Nota del estudiante: ", NOTA_MIN, NOTA_MAX);
-				int largoNombre = nombreEstudiante.getBytes().length;
-				try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, rw)) {
-					ficheroAlumnos.seek(ficheroAlumnos.length());
-					ficheroAlumnos.writeInt(contadorEstudiante);
-					ficheroAlumnos.writeUTF(nombreEstudiante);
-					for (int i = 0; i < (40 - largoNombre); i++) {
-						ficheroAlumnos.writeByte(0);
-					}
-					ficheroAlumnos.writeFloat(nota);
-					
-				} catch (IOException ioe) {
-					ES.msgErrln(errorEscritura);
-				}
+				nuevoEstudiante();
 				break;
 			}
 
 			case 2: {
-				try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, rw)) {
-					ficheroAlumnos.seek(0);
-					boolean encontrado = false;
-					int buscarID = ES.leeEntero("Introduzca el ID del estudiante: ");
-					
-					while (!encontrado && ficheroAlumnos.getFilePointer() < ficheroAlumnos.length()) {
-						int id = ficheroAlumnos.readInt();
-						
-						if (id == buscarID) {
-							encontrado = true;
-							String nombre = ficheroAlumnos.readUTF();
-							int largoNombre = nombre.getBytes().length;
-							ficheroAlumnos.skipBytes(40 - largoNombre);
-							float nota = ficheroAlumnos.readFloat();
-							ES.msgln("Se ha encontrado al alumno: \n" + "ID: " + id + "\n" + "Nombre: " + nombre + "\n"
-									+ "Nota: " + nota + "\n");
-						} else {
-							ficheroAlumnos.skipBytes(RECORD_SIZE - 2);
-						}
-					}
-					
-					if (!encontrado) {
-						ES.msgErrln(regNoExiste);
-					}
-					break;
-				} catch (IOException ioe) {
-					ES.msgErrln(errorLectura);
-				}
+				getEstudiantePorID();
+				break;
 			}
 			case 3: {
-				try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, rw)) {
-					ficheroAlumnos.seek(0);
-					boolean encontrado = false;
-					int buscarID = ES.leeEntero("Introduzca el ID del estudiante a modificar: ");
-					
-					while (!encontrado && ficheroAlumnos.getFilePointer() < ficheroAlumnos.length()) {
-						int id = ficheroAlumnos.readInt();
-						
-						if (id == buscarID) {
-							encontrado = true;
-							String nuevoNombre = ES.leeCadena("Nuevo nombre del alumno: ");
-							int largoNuevoNombre = nuevoNombre.getBytes().length;
-							float nuevaNota = ES.leeFloat("Nueva nota: ", NOTA_MIN, NOTA_MAX);
-							ficheroAlumnos.writeUTF(nuevoNombre);
-							ficheroAlumnos.skipBytes(40 - largoNuevoNombre);
-							ficheroAlumnos.writeFloat(nuevaNota);
-						} else {
-							ficheroAlumnos.skipBytes(RECORD_SIZE - 2);
-						}
-					}
-					
-					if (!encontrado) {
-						ES.msgErrln(regNoExiste);
-					}
-					break;
-				} catch (IOException ioe) {
-					ES.msgErrln(errorLectura);
-				}
-			}
+				modificarEstudiante();
 				break;
+			}
 			case 4: {
-				// TODO eliminar
-//				try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, rw)) {
-//					ficheroAlumnos.seek(0);
-//					boolean encontrado = false;
-//					int buscarID = ES.leeEntero("Introduzca el ID del estudiante a eliminar: ");
-//					
-//					while (!encontrado && ficheroAlumnos.getFilePointer() < ficheroAlumnos.length()) {
-//						int id = ficheroAlumnos.readInt();
-//						
-//						if (id == buscarID) {
-//							encontrado = true;
-//							
-//						} else {
-//							ficheroAlumnos.skipBytes(RECORD_SIZE - 2);
-//						}
-//					}
-//					
-//					if (!encontrado) {
-//						ES.msgErrln(regNoExiste);
-//					}
-//					break;
-//				} catch (IOException ioe) {
-//					ES.msgErrln(errorLectura);
-//				}
+				eliminarEstudiante();
 				break;
 			}
-				
+
 			case 5: {
-				try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, rw)) {
-					ficheroAlumnos.seek(0);
-					for (int i = 0; i < contadorEstudiante; i++) {
-						int id = ficheroAlumnos.readInt();
-						String nombre = ficheroAlumnos.readUTF();
-						int largoNombre = nombre.getBytes().length;
-						ficheroAlumnos.skipBytes(40 - largoNombre);
-						float nota = ficheroAlumnos.readFloat();
-
-						ES.msgln("ID: " + id + "\n" + "Nombre: " + nombre + "\n" + "Nota: " + nota + "\n");
-					}
-				} catch (IOException ioe) {
-					ES.msgErrln(errorLectura);
-				}
-
+				listarEstudiantes();
 				break;
 			}
-			case 6:
+
+			case 6: {
 				ES.msgln("Saliendo...");
+				salir = true;
 				break;
-			default:
+			}
+			default: {
 				ES.msgErrln("Opción inválida.");
 				break;
+			}
 			}
 		}
 	}
 
+	static int contadorEstudiantes = getNumeroElementos();
 	static final int RECORD_SIZE = 48;
 	static final float NOTA_MAX = 10.0f;
 	static final float NOTA_MIN = 0.0f;
@@ -167,18 +68,156 @@ public class EjercicioAccDir {
 	static final String errorLectura = "No se ha podido leer los datos";
 	static final String regNoExiste = "El alumno no existe.";
 	static final String fichero = "estudiantes.db";
-	static final String rw = "rw";
+	static final String RO = "r";
+	static final String RW = "rw";
 
 	private static int getNumeroElementos() {
 		int numeroElementos = 0;
-		try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, rw)) {
+		try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, RW)) {
 			long tamFichero = ficheroAlumnos.length();
 			numeroElementos = (int) (tamFichero / RECORD_SIZE);
-			
+
 		} catch (IOException ioe) {
-			
+			ES.msgErrln("Error al intentar conseguir el número de elementos");
 		}
-		
+
 		return numeroElementos;
+	}
+
+	private static void nuevoEstudiante() {
+		contadorEstudiantes++;
+		String nombreEstudiante = ES.leeCadena("Nombre del estudiante: ");
+		float nota = ES.leeFloat("Nota del estudiante: ", NOTA_MIN, NOTA_MAX);
+		int largoNombre = nombreEstudiante.getBytes().length;
+		try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, RW)) {
+			ficheroAlumnos.seek(ficheroAlumnos.length());
+			ficheroAlumnos.writeInt(contadorEstudiantes);
+			ficheroAlumnos.writeUTF(nombreEstudiante);
+			for (int i = 0; i < (40 - largoNombre); i++) {
+				ficheroAlumnos.writeByte(0);
+			}
+			ficheroAlumnos.writeFloat(nota);
+
+		} catch (IOException ioe) {
+			ES.msgErrln(errorEscritura);
+		}
+	}
+
+	private static void getEstudiantePorID() {
+		try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, RO)) {
+			ficheroAlumnos.seek(0);
+			boolean encontrado = false;
+			int buscarID = ES.leeEntero("Introduzca el ID del estudiante: ");
+
+			while (!encontrado && ficheroAlumnos.getFilePointer() < ficheroAlumnos.length()) {
+				int id = ficheroAlumnos.readInt();
+
+				if (id == buscarID) {
+					encontrado = true;
+					String nombre = ficheroAlumnos.readUTF();
+					int largoNombre = nombre.getBytes().length;
+					ficheroAlumnos.skipBytes(40 - largoNombre);
+					float nota = ficheroAlumnos.readFloat();
+					ES.msgln("Se ha encontrado al alumno con ID: " + id + "\n" + "Nombre: " + nombre + "\n" + "Nota: "
+							+ nota + "\n");
+				} else {
+					ficheroAlumnos.skipBytes(RECORD_SIZE - 2);
+				}
+			}
+
+			if (!encontrado) {
+				ES.msgErrln(regNoExiste);
+			}
+
+		} catch (IOException ioe) {
+			ES.msgErrln(errorLectura);
+		}
+	}
+
+	private static void modificarEstudiante() {
+		try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, RW)) {
+			ficheroAlumnos.seek(0);
+			boolean encontrado = false;
+			int buscarID = ES.leeEntero("Introduzca el ID del estudiante a modificar: ");
+
+			while (!encontrado && ficheroAlumnos.getFilePointer() < ficheroAlumnos.length()) {
+				int id = ficheroAlumnos.readInt();
+
+				if (id == buscarID) {
+					encontrado = true;
+					String nuevoNombre = ES.leeCadena("Nuevo nombre del alumno: ");
+					int largoNuevoNombre = nuevoNombre.getBytes().length;
+					float nuevaNota = ES.leeFloat("Nueva nota: ", NOTA_MIN, NOTA_MAX);
+					ficheroAlumnos.writeUTF(nuevoNombre);
+					ficheroAlumnos.skipBytes(40 - largoNuevoNombre);
+					ficheroAlumnos.writeFloat(nuevaNota);
+				} else {
+					ficheroAlumnos.skipBytes(RECORD_SIZE - 2);
+				}
+			}
+
+			if (!encontrado) {
+				ES.msgErrln(regNoExiste);
+			}
+
+		} catch (IOException ioe) {
+			ES.msgErrln(errorLectura);
+		}
+	}
+
+	private static void listarEstudiantes() {
+		try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, RO)) {
+			ficheroAlumnos.seek(0);
+			for (int i = 0; i <= contadorEstudiantes; i++) {
+				int id = ficheroAlumnos.readInt();
+				if (id != -1) {
+					String nombre = ficheroAlumnos.readUTF();
+					int largoNombre = nombre.getBytes().length;
+					ficheroAlumnos.skipBytes(40 - largoNombre);
+					float nota = ficheroAlumnos.readFloat();
+
+					ES.msgln("ID: " + id + "\n" + "Nombre: " + nombre + "\n" + "Nota: " + nota + "\n");
+				} else {
+					ficheroAlumnos.skipBytes(RECORD_SIZE - 2);
+				}
+			}
+		} catch (EOFException eof) {
+
+		} catch (IOException ioe) {
+			ES.msgErrln(errorLectura);
+			ioe.printStackTrace();
+		}
+	}
+
+	private static void eliminarEstudiante() {
+
+		try (RandomAccessFile ficheroAlumnos = new RandomAccessFile(fichero, RW)) {
+			ficheroAlumnos.seek(0);
+			boolean encontrado = false;
+			int buscarID = ES.leeEntero("Introduzca el ID del estudiante a eliminar: ");
+
+			while (!encontrado && ficheroAlumnos.getFilePointer() < ficheroAlumnos.length()) {
+				int id = ficheroAlumnos.readInt();
+
+				if (id == buscarID) {
+					encontrado = true;
+					ficheroAlumnos.seek(ficheroAlumnos.getFilePointer() - 4);
+					ficheroAlumnos.writeInt(-1);
+					int bytesEliminado = "eliminado".getBytes().length;
+					ficheroAlumnos.writeUTF("eliminado");
+					ficheroAlumnos.skipBytes(40 - bytesEliminado);
+					ficheroAlumnos.writeFloat(-1f);
+				} else {
+					ficheroAlumnos.skipBytes(RECORD_SIZE - 2);
+				}
+			}
+
+			if (!encontrado) {
+				ES.msgErrln(regNoExiste);
+			}
+
+		} catch (IOException ioe) {
+			ES.msgErrln(errorLectura);
+		}
 	}
 }
