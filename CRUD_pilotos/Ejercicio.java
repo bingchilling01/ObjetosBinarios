d72ae56ee21547dda4d9a244d4cd86d4ee3ad747
Lin <126698830+bingchilling01@users.noreply.github.com>
2023-11-20 20:44:22 +0100

    Update Ejercicio.java

--- CRUD_pilotos/Ejercicio.java
+++ CRUD_pilotos/Ejercicio.java
@@ -13,8 +13,8 @@ public class Ejercicio {
         boolean salir = false;
 
         while (!salir) {
-            ES.msgln("1. Insertar un piloto\n2. Leer lista de pilotos\n3. Modificar piloto\n4. Eliminar piloto");
-            ES.msgln("5. Insertar escuderìa\n6. Leer Lista de escuderías\n7. Modificar escudería\n8. Eliminar escudería");
+            ES.msgln("1. Insertar un piloto\n2. Mostrar lista de pilotos\n3. Modificar piloto\n4. Eliminar piloto");
+            ES.msgln("5. Insertar escuderìa\n6. Mostrar lista de escuderías\n7. Modificar escudería\n8. Eliminar escudería\n0. Salir");
             opcion = ES.leeEntero("Opción: ");
             switch (opcion) {
                 case 1: {
@@ -28,13 +28,58 @@ public class Ejercicio {
                 }
 
                 case 2: {
-                    consultaSQL();
+                    consultaPilotos();
                     break;
                 }
 
                 case 3: {
-                    int idBuscado = ES.leeEntero("ID del piloto a cambiar: ");
-                    actualizaSQL(idBuscado);
+                    int idPilotoBuscado = ES.leeEntero("ID del piloto a actualizar: ");
+                    actualizaPiloto(idPilotoBuscado);
+                    break;
+                }
+
+                case 4: {
+                    int idPilotoBuscado = ES.leeEntero("ID del piloto a eliminar: ");
+                    eliminaPiloto(idPilotoBuscado);
+                    break;
+                }
+
+                case 5: {
+                    String nombre = ES.leeCadena("Nombre de la escudería: ");
+                    insertarEscuderia(nombre);
+                    break;
+                }
+
+                case 6: {
+                    consultaEscuderias();
+                    break;
+                }
+
+                case 7: {
+                    int idEscuderiaBuscada = ES.leeEntero("ID de la escudería a actualizar: ");
+                    actualizaEscuderia(idEscuderiaBuscada);
+                    break;
+                }
+
+                case 8: {
+                    int idEscuderiaBuscada = ES.leeEntero("ID de la escudería a eliminar: ");
+                    eliminaEscuderia(idEscuderiaBuscada);
+                    break;
+                }
+
+                case 0: {
+                    salir = true;
+                    ES.msgln("Saliendo del programa...");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException ie) {
+
+                    }
+                    break;
+                }
+
+                default: {
+                    ES.msgErrln("Opción no válida");
                 }
 
             }
@@ -42,6 +87,8 @@ public class Ejercicio {
 
 
     }
+
+
 
     private static void insertarPiloto(String nombre, String apellidos, int escuderia) {
         try {
@@ -64,7 +111,7 @@ public class Ejercicio {
             e.printStackTrace();
         }
     }
-    private static void consultaSQL() {
+    private static void consultaPilotos() {
         try {
             // Cargar el driver
             Class.forName(claseDriver);
@@ -79,11 +126,16 @@ public class Ejercicio {
 
             // Recorremos el resultado para visualizar cada fila
             // Se hace un bucle mientras haya registros y se van visualizando
-            System.out.println("Lista de pilotos de la BBDD:");
-            while (datosResultantes.next()) {
-                ES.msgln("-------------------------------------------------------------------");
-                System.out.println("ID: " + datosResultantes.getInt(1) + " Nombre: " + datosResultantes.getString(2) + " Apellidos: " + datosResultantes.getString(3) + " Escudería: " + datosResultantes.getInt(4));
-                ES.msgln("-------------------------------------------------------------------");
+
+            if(datosResultantes.next()) {
+                ES.msgln("Lista de pilotos de la BBDD:");
+                do {
+                    ES.msgln("-------------------------------------------------------------------");
+                    ES.msgln("ID: " + datosResultantes.getInt(1) + " Nombre: " + datosResultantes.getString(2) + " Apellidos: " + datosResultantes.getString(3) + " Escudería: " + datosResultantes.getInt(4));
+                    ES.msgln("-------------------------------------------------------------------");
+                } while (datosResultantes.next());
+            } else {
+                ES.msgErrln("La lista de pilotos está vacía.");
             }
 
             datosResultantes.close(); // Cerrar ResultSet
@@ -97,7 +149,7 @@ public class Ejercicio {
         }
     }
 
-    private static void actualizaSQL(int idBuscado) {
+    private static void actualizaPiloto(int idBuscado) {
         try {
             // Cargar el driver
             Class.forName(claseDriver);
@@ -113,6 +165,7 @@ public class Ejercicio {
                 int escuderiaNueva = ES.leeEntero("Escudería nueva: ");
 
                 sentencia.executeUpdate("UPDATE piloto SET " + "nombre = '" + nombreNuevo + "', apellidos = '" + apellidosNuevos + "', id_escuderia = " + escuderiaNueva + " WHERE id = " + idBuscado + ";");
+                ES.msgln("Piloto actualizado");
             } else {
                 ES.msgErrln("No se ha encontrado el piloto");
             }
@@ -126,4 +179,155 @@ public class Ejercicio {
             e.printStackTrace();
         }
     }
+
+    private static void eliminaPiloto(int idBuscado) {
+        try {
+            // Cargar el driver
+            Class.forName(claseDriver);
+
+            // Establecemos la conexion con la BD
+            Connection conexion = DriverManager.getConnection(paramConexion[0], paramConexion[1], paramConexion[2]);
+
+            // Preparamos la consulta
+            Statement sentencia = conexion.createStatement();
+            if(sentencia.executeQuery("SELECT nombre FROM piloto WHERE id = " + idBuscado).next()) {
+
+                sentencia.executeUpdate("DELETE FROM piloto WHERE id = " + idBuscado);
+                ES.msgln("Piloto eliminado");
+
+            } else {
+                ES.msgErrln("No se ha encontrado el piloto");
+            }
+
+            sentencia.close(); // Cerrar Statement
+            conexion.close(); // Cerrar conexión
+
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertarEscuderia(String nombre) {
+        try {
+            // Cargar el driver
+            Class.forName(claseDriver);
+
+            // Establecemos la conexion con la BD
+            Connection conexion = DriverManager.getConnection(paramConexion[0], paramConexion[1], paramConexion[2]);
+
+            // Preparamos la consulta
+            Statement sentencia = conexion.createStatement();
+            sentencia.executeUpdate("INSERT INTO escuderia VALUES (0, '" + nombre + "';");
+
+            sentencia.close(); // Cerrar Statement
+            conexion.close(); // Cerrar conexión
+
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void consultaEscuderias() {
+        try {
+            // Cargar el driver
+            Class.forName(claseDriver);
+
+            // Establecemos la conexion con la BD
+            Connection conexion = DriverManager.getConnection(paramConexion[0], paramConexion[1], paramConexion[2]);
+
+            // Preparamos la consulta
+            Statement sentencia = conexion.createStatement();
+//            sentencia.executeUpdate("INSERT INTO departamentos VALUES (0, 'Logística');");
+            ResultSet datosResultantes = sentencia.executeQuery("SELECT * FROM escuderia;");
+
+            // Recorremos el resultado para visualizar cada fila
+            // Se hace un bucle mientras haya registros y se van visualizando
+
+            if(datosResultantes.next()) {
+                ES.msgln("Lista de escuderias de la BBDD:");
+                do {
+                    ES.msgln("-------------------------------------------------------------------");
+                    ES.msgln("ID: " + datosResultantes.getInt(1) + " Nombre: " + datosResultantes.getString(2));
+                    ES.msgln("-------------------------------------------------------------------");
+                } while (datosResultantes.next());
+            } else {
+                ES.msgErrln("La lista de escuderías está vacía");
+            }
+
+
+
+            datosResultantes.close(); // Cerrar ResultSet
+            sentencia.close(); // Cerrar Statement
+            conexion.close(); // Cerrar conexión
+
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void actualizaEscuderia(int idBuscado) {
+        try {
+            // Cargar el driver
+            Class.forName(claseDriver);
+
+            // Establecemos la conexion con la BD
+            Connection conexion = DriverManager.getConnection(paramConexion[0], paramConexion[1], paramConexion[2]);
+
+            // Preparamos la consulta
+            Statement sentencia = conexion.createStatement();
+            if(sentencia.executeQuery("SELECT nombre FROM escuderia WHERE id = " + idBuscado).next()) {
+                String nombreNuevo = ES.leeCadena("Nuevo nombre: ");
+
+                sentencia.executeUpdate("UPDATE escuderia SET " + "nombre = '" + nombreNuevo + "';");
+                ES.msgln("Piloto actualizado");
+            } else {
+                ES.msgErrln("No se ha encontrado la escudería");
+            }
+
+            sentencia.close(); // Cerrar Statement
+            conexion.close(); // Cerrar conexión
+
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void eliminaEscuderia(int idBuscado) {
+        try {
+            // Cargar el driver
+            Class.forName(claseDriver);
+
+            // Establecemos la conexion con la BD
+            Connection conexion = DriverManager.getConnection(paramConexion[0], paramConexion[1], paramConexion[2]);
+
+            // Preparamos la consulta
+            Statement sentencia = conexion.createStatement();
+            if(sentencia.executeQuery("SELECT nombre FROM escuderia WHERE id = " + idBuscado).next()) {
+
+                sentencia.executeUpdate("DELETE FROM escuderia WHERE id = " + idBuscado);
+                ES.msgln("Escudería eliminada");
+
+            } else {
+                ES.msgErrln("No se ha encontrado la escudería");
+            }
+
+            sentencia.close(); // Cerrar Statement
+            conexion.close(); // Cerrar conexión
+
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
